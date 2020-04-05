352e62235044805dabbd28f1e468e22b58d7b285
Evan Bernstein <eeevanbbb@me.com>
2020-04-05 12:14:23 -0700

    Get the auth() object directly from firebase

--- projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
+++ projects/firebaseui-angular-library/src/lib/firebaseui-angular-library.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import * as firebaseui from 'firebaseui';
+import * as firebase from "firebase";
 
 @Injectable()
 export class FirebaseuiAngularLibraryService {
@@ -9,7 +10,7 @@ export class FirebaseuiAngularLibraryService {
   constructor(angularFireAuth: AngularFireAuth) {
     // store the firebaseui instance on the window object to prevent double initialization
     if (!(<any>window).firebaseUiInstance) {
-      (<any>window).firebaseUiInstance = new firebaseui.auth.AuthUI(angularFireAuth.auth);
+      (<any>window).firebaseUiInstance = new firebaseui.auth.AuthUI(firebase.auth());
     }
     this.firebaseUiInstance = (<any>window).firebaseUiInstance as firebaseui.auth.AuthUI;
   }
